fe49db37cc7481d801ec81e6b83f2fb1e5db90c2
Sergio Ocón-Cárdenas <sergio.ocon@gmail.com>
2024-11-04 09:33:55 +0100

    adding presence

--- test/counter_web/live/counter_live_test.exs
+++ test/counter_web/live/counter_live_test.exs
@@ -24,4 +24,34 @@ defmodule CounterWeb.CounterLiveTest do
     send(view.pid, {:count, 2})
     assert render(view) =~ "Counter: 2"
   end
+
+  test "handle_info/2 Presence Update - Joiner", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/")
+    assert html =~ "Connected Clients: 1"
+
+    send(view.pid, %{
+      event: "presence_diff",
+      payload: %{
+        joins: %{"phx-Fhb_dqdqsOCzKQAl" => %{metas: [%{phx_ref: "Fhb_dqdrwlCmfABl"}]}},
+        leaves: %{}
+      }
+    })
+
+    assert render(view) =~ "Connected Clients: 2"
+  end
+
+  test "handle_info/2 Presence Update - Leaver", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/")
+    assert html =~ "Connected Clients: 1"
+
+    send(view.pid, %{
+      event: "presence_diff",
+      payload: %{
+        joins: %{},
+        leaves: %{"phx-Fhb_dqdqsOCzKQAl" => %{metas: [%{phx_ref: "Fhb_dqdrwlCmfABl"}]}}
+      }
+    })
+
+    assert render(view) =~ "Connected Clients: 0"
+  end
 end
